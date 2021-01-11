46581d761fde30c77ab1a06864e321fce07fbb54
baskuunk <kasbuunk@me.com>
2021-01-11 13:01:22 +0100

    changed text for testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Credit <code>src/App.js</code> and save to reload.
+          Schmedit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
